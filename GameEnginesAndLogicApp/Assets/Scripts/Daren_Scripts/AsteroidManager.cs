a8b138f5be80eab0a8f654aa25ec6294c13be2ee
QA3R <46014750+QA3R@users.noreply.github.com>
2020-03-23 14:04:14 -0400

    Added ASteroid Functionality

---
+++ GameEnginesAndLogicApp/Assets/Scripts/Daren_Scripts/AsteroidManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AsteroidManager : MonoBehaviour
+{
+    // Spawn boundaries for new asteroids
+    public float minSpawnPosY;
+    public float maxSpawnPosY;
+
+    ObjectPooler objectPooler;
+ 
+
+    public IEnumerator AsteroidSpawner()
+    {
+        objectPooler.SpawnFromPool("Asteroid", new Vector3(transform.position.x, Random.Range(minSpawnPosY, maxSpawnPosY), transform.position.z), Quaternion.identity);
+        yield return new WaitForSeconds(Random.Range(1,2));
+        StartCoroutine("AsteroidSpawner");
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        objectPooler = ObjectPooler.Instance; 
+        StartCoroutine("AsteroidSpawner");
+    }
+
+    void FixedUpdate()
+    {
+
+    }
+}
