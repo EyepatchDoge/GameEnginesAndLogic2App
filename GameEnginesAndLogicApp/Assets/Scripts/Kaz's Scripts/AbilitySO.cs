b3c33b715332885df590cad6f793a3d09a244a23
EyepatchDoge <kaspo.eyepatch@gmail.com>
2020-04-04 14:46:18 -0400

    Kascaron_vfx graph
    added the package for vfx graph to begin making particles systems for game

--- GameEnginesAndLogicApp/Assets/Scripts/Kaz's Scripts/AbilitySO.cs
+++ GameEnginesAndLogicApp/Assets/Scripts/Kaz's Scripts/AbilitySO.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+//createda SO for abilities to use in ability manager and purchases
 [CreateAssetMenu(fileName = "AbilitySO", menuName = "ScriptableObjects/Abilities", order = 2)]
 public class AbilitySO : ScriptableObject
 {
