0fc88d765e76c221014e22db249cf1d821249539
SynPyre <58309964+SynPyre@users.noreply.github.com>
2020-04-04 17:19:20 -0400

    AudioManger

--- GameEnginesAndLogicApp/Assets/Scripts/Julienne_Scripts/AudioManager.cs
+++ GameEnginesAndLogicApp/Assets/Scripts/Julienne_Scripts/AudioManager.cs
@@ -1,18 +1,30 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using UnityEngine.Audio;
+using System;
 
 public class AudioManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    
+    public Sounds[] sounds;
+
+    void Awake()
     {
-        
+        foreach (Sounds s in sounds)
+        {
+            s.source= gameObject.AddComponent<AudioSource>();
+            s.source.clip = s.clip;
+
+            s.source.volume = s.volume;
+            s.source.pitch = s.pitch;
+
+
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    public void Play (string name)
     {
-        
+       Sounds s = Array.Find(sounds, sound => sound.name == name);
+
     }
+
 }
--- GameEnginesAndLogicApp/Assets/Scripts/Julienne_Scripts/PauseGame.cs
+++ GameEnginesAndLogicApp/Assets/Scripts/Julienne_Scripts/PauseGame.cs
@@ -1,16 +1,16 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class PauseGame : MonoBehaviour
 {
     public void Pause()
     {
+        FindObjectOfType<AudioManager>().Play("PauseButton");
         Time.timeScale = 0;
     }
 
    public void Resume()
     {
+        FindObjectOfType<AudioManager>().Play("ResumeButton");
         Time.timeScale = 1;
     }
 }
