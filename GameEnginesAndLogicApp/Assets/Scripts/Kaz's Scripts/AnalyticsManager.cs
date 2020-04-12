f8b98e3b8b604a93aea7a347e7b895c48f73b481
EyepatchDoge <kaspo.eyepatch@gmail.com>
2020-04-11 21:56:33 -0400

    kas_update19
    - added analytics to game

---
+++ GameEnginesAndLogicApp/Assets/Scripts/Kaz's Scripts/AnalyticsManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Analytics;
+
+public class AnalyticsManager : MonoBehaviour
+{
+    public void PurchaseShield()
+    {
+        Analytics.CustomEvent("Shield Purchased");
+    }
+
+    public void PurchaseLight()
+    {
+        Analytics.CustomEvent("Light Purchased");
+    }
+
+    public void FiveHundredSpaceCoins()
+    {
+        Analytics.CustomEvent("500 Space coins purchased");
+    }
+}
